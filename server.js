a35262fd9d4a42ea55fbed57b6974723d4285d88
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-03 14:23:30 +0300

    initial routing setup

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
 
 const app = express()
+
+app.get('/api/v1/bootcamps',(req,res)=>{
+    res.status(200).json({success: true,msg: 'Show all botcamps'});
+})
+
+app.post('/api/v1/bootcamps',(req,res)=>{
+    res.status(200).json({success: true,msg: 'Create new botcamps'});
+})
+
+app.put('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({success: true,msg: `Update botcamp ${req.params.id}`});
+})
+
+app.get('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({success: true,msg: `Show botcamp ${req.params.id}`});
+})
+
+
+app.delete('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({success: true,msg: `Delete botcamp ${req.params.id}`});
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ))

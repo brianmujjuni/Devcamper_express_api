81e7f5cebc62494bd65185a96ac04d13d0b58047
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-03 14:35:17 +0300

    initial route setup

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
+
 //load env vars
 dotenv.config({path: './config/config.env'})
 
 const app = express()
 
-app.get('/api/v1/bootcamps',(req,res)=>{
-    res.status(200).json({success: true,msg: 'Show all botcamps'});
-})
-
-app.post('/api/v1/bootcamps',(req,res)=>{
-    res.status(200).json({success: true,msg: 'Create new botcamps'});
-})
-
-app.put('/api/v1/bootcamps/:id',(req,res)=>{
-    res.status(200).json({success: true,msg: `Update botcamp ${req.params.id}`});
-})
-
-app.get('/api/v1/bootcamps/:id',(req,res)=>{
-    res.status(200).json({success: true,msg: `Show botcamp ${req.params.id}`});
-})
-
-
-app.delete('/api/v1/bootcamps/:id',(req,res)=>{
-    res.status(200).json({success: true,msg: `Delete botcamp ${req.params.id}`});
-})
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000
 

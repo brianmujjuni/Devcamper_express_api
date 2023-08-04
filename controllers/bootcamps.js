b662c6586a29dd5b501855db6b805dcdba9f7fd5
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-04 11:06:47 +0300

    added fetch all data

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,46 +3,55 @@ const Bootcamp = require('../Models/Bootcamp')
 
 // @route Get /api/v1/bootcamps
 //@access Public
-exports.getBootCamps = (req,res,next) =>{
-    res.status(200).json({success: true,msg: 'Show all botcamps'});
+exports.getBootCamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({
+            success: true,
+            data: bootcamps
+        })
+    } catch (error) {
+        res.status(400).json({ success: false })
+    }
+
 }
 
 // @desc get single bootcamps
 // @route Get /api/v1/bootcamps/:id
 //@access Public
-exports.getBootCamp = (req,res,next) =>{
-    res.status(200).json({success: true,msg: `Show botcamp ${req.params.id}`});
+exports.getBootCamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Show botcamp ${req.params.id}` });
 }
 
 // @desc create new bootcamps
 // @route Post /api/v1/bootcamps
 //@access Private
-exports.createBootCamp = async (req,res,next) =>{
+exports.createBootCamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.create(req.body)
         res.status(201).json({
-          success:  true,
-          data: bootcamp
-        }) 
+            success: true,
+            data: bootcamp
+        })
     } catch (error) {
         res.status(400).json({
             success: false,
-           
+
         })
     }
-  
+
 }
 
 // @desc update  bootcamps
 // @route Put /api/v1/bootcamps/:id
 //@access Private
-exports.updateBootCamp = (req,res,next) =>{
-    res.status(200).json({success: true,msg: `Update botcamp ${req.params.id}`});
+exports.updateBootCamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Update botcamp ${req.params.id}` });
 }
 
 // @desc delete bootcamps
 // @route Delete /api/v1/bootcamps/:id
 //@access Private
-exports.deleteBootCamp = (req,res,next) =>{
-    res.status(200).json({success: true,msg: `Delete botcamp ${req.params.id}`});
+exports.deleteBootCamp = (req, res, next) => {
+    res.status(200).json({ success: true, msg: `Delete botcamp ${req.params.id}` });
 }

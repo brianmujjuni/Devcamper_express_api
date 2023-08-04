d0efff7ff642a2ac41b17cd78fd90be912322a2c
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-04 10:55:37 +0300

    added error handling to create bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -18,11 +18,19 @@ exports.getBootCamp = (req,res,next) =>{
 // @route Post /api/v1/bootcamps
 //@access Private
 exports.createBootCamp = async (req,res,next) =>{
-  const bootcamp = await Bootcamp.create(req.body)
-  res.status(201).json({
-    success:  true,
-    data: bootcamp
-  })
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({
+          success:  true,
+          data: bootcamp
+        }) 
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+           
+        })
+    }
+  
 }
 
 // @desc update  bootcamps

80276286fda28b5f7e29737c9e10a22d7876721d
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-08 16:07:54 +0300

    added ddrop data and create seeder data

--- seeder.js
+++ seeder.js
@@ -12,10 +12,35 @@ const Bootcamp = require('./Models/Bootcamp')
 //connect to db
 mongoose.connect(process.env.MONGO_URI,{
 useNewUrlParser: true,
-useCreateIndex: true,
-useFindAndModify: false,
 useUnifiedTopology: true
 });
 
 //read json files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/data/bootcamps.json`,'utf-8'))
+
+const importData = async ()=>{
+    try {
+        await Bootcamp.create(bootcamps)
+        console.log('Data imported'.green.inverse)
+        process.exit
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//delete data
+const deleteData = async ()=>{
+    try {
+        await Bootcamp.deleteMany(bootcamps)
+        console.log('Data Destroyed'.red.inverse)
+        process.exit
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+if(process.argv[2] === '-i'){
+    importData()
+}else if(process.argv[2] === '-d'){
+    deleteData()
+}

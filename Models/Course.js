08049b5d3a0fead0ff89693ac626286597b8c11f
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-12 15:23:53 +0300

    added aggregation

--- Models/Course.js
+++ Models/Course.js
@@ -4,40 +4,75 @@ const CourseSchema = new mongoose.Schema({
     title: {
         type: String,
         trim: true,
-        required: [true,'Please add a course title']
+        required: [true, 'Please add a course title']
     },
-    description:{
+    description: {
         type: String,
-        required: [true,'Please add a description']
+        required: [true, 'Please add a description']
     },
-    weeks:{
-        type:String,
-        required: [true,'Please add number of weeks']
-    },
-    tuition:{
-        type:Number,
-        required:[true,'Please add a tuition cost']
-    },
-    minimumSkill:{
+    weeks: {
         type: String,
-        required:[true,'Please add a minimum skill'],
-        enum: ['beginner','intermediate','advanced']
+        required: [true, 'Please add number of weeks']
     },
-    scholarschipAvailable:{
+    tuition: {
+        type: Number,
+        required: [true, 'Please add a tuition cost']
+    },
+    minimumSkill: {
+        type: String,
+        required: [true, 'Please add a minimum skill'],
+        enum: ['beginner', 'intermediate', 'advanced']
+    },
+    scholarschipAvailable: {
         type: Boolean,
-       default: false
-       
+        default: false
+
     },
-    createdAt:{
+    createdAt: {
         type: Date,
         default: Date.now
     },
     //creating relationship
-    bootcamp:{
+    bootcamp: {
         type: mongoose.Schema.ObjectId,
-        ref:'Bootcamp',
-        required:true
+        ref: 'Bootcamp',
+        required: true
     }
 })
 
-module.exports = mongoose.model('Course',CourseSchema)
+//static method to get avg of course tuitions
+CourseSchema.static.getAverageCost = async function (bootcampId) {
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group:{
+                _id: '$bootcamp',
+                averageCost: {$avg: '$tuition'}
+            }
+        }
+    ])
+
+    try{
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId,{
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+//Call getaverage coset after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp)
+})
+
+//Call getaverage coset before remove
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp) 
+})
+
+module.exports = mongoose.model('Course', CourseSchema)
--- seeder.js
+++ seeder.js
@@ -23,7 +23,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/data/courses.json`,'utf
 const importData = async ()=>{
     try {
         await Bootcamp.create(bootcamps)
-        await Course.create(courses)
+       // await Course.create(courses)
         console.log('Data imported'.green.inverse)
         process.exit
     } catch (error) {

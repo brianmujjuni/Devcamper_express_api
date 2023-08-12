d17f97976f01d736978391f94d8c49f67b337504
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-12 10:34:41 +0300

    added populate of bootcamp to course

--- controllers/courses.js
+++ controllers/courses.js
@@ -5,13 +5,16 @@ const asyncHandler = require('../middleware/async')
 //get all Courses
 // GET /api/v1/courses/
 // GET api/v1/bootcamps/:bootcampId/courses
-exports.getCourses = asyncHandler(async(req,res,next)=>{
+exports.getCourses = asyncHandler(async (req, res, next) => {
     let query;
 
-    if(req.params.bootcampId){
-       query = Course.find({bootcamp: req.params.bootcampId}) 
-    }else{
-        query = Course.find()
+    if (req.params.bootcampId) {
+        query = Course.find({ bootcamp: req.params.bootcampId })
+    } else {
+        query = Course.find().populate({
+            path: 'bootcamp',
+            select: 'name description'
+        })
     }
 
     const courses = await query

174dba6a2e960a41459e644512f8f00afa126a3a
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-14 15:39:54 +0300

    added pagination to courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,15 +8,17 @@ const ErrorResponse = require('../utils/errorResponse')
 // GET /api/v1/courses/
 // GET api/v1/bootcamps/:bootcampId/courses
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    let query;
 
     if (req.params.bootcampId) {
-        query = Course.find({ bootcamp: req.params.bootcampId })
+       const courses = await Course.find({ bootcamp: req.params.bootcampId })
+
+       return res.status(200).json({
+        success:true,
+        count: courses.length,
+        data: courses
+       })
     } else {
-        query = Course.find().populate({
-            path: 'bootcamp',
-            select: 'name description'
-        })
+        res.status(200).json(res.advancedResults)
     }
 
     const courses = await query
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,16 @@
 
 const express = require('express')
 const {getCourses, getCourse,addCourse,updateCorse,deleteCourse} = require('../controllers/courses')
+const Course = require('../Models/Course')
+const advancedRequest = require('../middleware/advancedRequest')
 
 const router = express.Router({mergeParams: true})
 
-router.route('/').get(getCourses).post(addCourse)
+router.route('/').get(advancedRequest(Course,{
+    path: 'bootcamp',
+    select: 'name description'
+}),getCourses).post(addCourse)
+
 router.route('/:id').get(getCourse).put(updateCorse).delete(deleteCourse)
 
 module.exports = router

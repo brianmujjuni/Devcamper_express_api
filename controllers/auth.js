a9e627e57f1a79e18d01a2140cd52634304623b6
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-21 11:21:33 +0300

    added create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,13 @@ const asyncHandler = require('../middleware/async')
 //public
 
 exports.register =asyncHandler(async(req,res,next)=>{
+    const {name,email,password,role} = req.body
+
+    //create use
+    const user = await User.create({name,email,password,role})
+
     res.status(200).json({
-        success: true
+        success: true,
+       data: user
     })
 })

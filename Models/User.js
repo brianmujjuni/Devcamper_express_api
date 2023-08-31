9f7e1f49585275ecf349302f4456b79e87ce35d5
Mujjuni Brian <brianmujjuni@engineer.com>
2023-08-31 09:37:21 +0300

    added reset password token to user model with crypto

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -48,6 +49,20 @@ userSchema.methods.getSignedJWTToken = function(){
 //match user entered password to hashed password in database on login
 userSchema.methods.matchPassword =async function(enteredPassword){
     return await bcrypt.compare(enteredPassword,this.password)
+}
+
+//Generate and hash password token
+userSchema.methods.getResetPasswordToken = function(){
+    //generate token
+    const resetToken = crypto.randomBytes(20).toString('hex')
+
+    //Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    //Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000  
+
+    return resetToken
 }
 
 module.exports = mongoose.model('User', userSchema)

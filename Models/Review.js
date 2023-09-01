b9e411686df27c8d536b60fa14fa94ca6851a89a
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-01 16:23:04 +0300

    modified reviews model

--- Models/Review.js
+++ Models/Review.js
@@ -34,7 +34,7 @@ const ReviewSchema = new mongoose.Schema({
 
 })
 
-//limit user from adding more than one review per bootcamp
-ReviewSchema.index({bootcamp: 1,user: 1},{unique: true})
+// Prevent user from submitting more than one review per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
 module.exports = mongoose.model('Review',ReviewSchema)

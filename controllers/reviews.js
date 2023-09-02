3735bbd11a8581cfdb0eaeac29780c3edac1bb45
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-02 12:01:37 +0300

    added update review function in controlller and rout

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -57,3 +57,30 @@ exports.addReview = asyncHandler(async (req, res, next) => {
         data: review
     })
 })
+
+//@desc Update review
+//@route Put /api/v1/reviews/:id
+//@access Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+    
+
+    let review = await Review.findById(req.params.id)
+
+    if(!review){
+        return next(new ErrorResponse(`No review found with id ${req.params.id}`,404))
+    }
+    //Make sure review belongs to user or user is admin
+   if(review.user.toString() !== req.user.id && req.user.role !== 'admin'){
+    return next(new ErrorResponse(`Not authorised to update review`,401))
+   }
+
+   review = await Review.findByIdAndUpdate(req.params.id,req.body,{
+    new: true,
+    runValidators: true
+   })
+
+    res.status(200).json({
+        success: true,
+        data: review
+    })
+})

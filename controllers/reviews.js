47ce4c17eab3fd06481811ea19d4a2d94373aea7
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-02 12:42:00 +0300

    added delete review function in review controller and delete route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -31,8 +31,8 @@ exports.getReview = asyncHandler(async (req, res, next) => {
         path: 'bootcamp',
         select: 'name description'
     })
-    if(!review){
-        return next(new ErrorResponse(`No review found with the id of ${req.params.id}`,404))
+    if (!review) {
+        return next(new ErrorResponse(`No review found with the id of ${req.params.id}`, 404))
     }
     res.status(200).json({
         success: true,
@@ -48,8 +48,8 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     req.body.user = req.user.id
 
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
-    if(!bootcamp){
-        return next(new ErrorResponse(`No bootcamp found with id ${req.params.bootcampId}`,404))
+    if (!bootcamp) {
+        return next(new ErrorResponse(`No bootcamp found with id ${req.params.bootcampId}`, 404))
     }
     const review = await Review.create(req.body)
     res.status(201).json({
@@ -62,25 +62,51 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 //@route Put /api/v1/reviews/:id
 //@access Private
 exports.updateReview = asyncHandler(async (req, res, next) => {
-    
+
 
     let review = await Review.findById(req.params.id)
 
-    if(!review){
-        return next(new ErrorResponse(`No review found with id ${req.params.id}`,404))
+    if (!review) {
+        return next(new ErrorResponse(`No review found with id ${req.params.id}`, 404))
     }
     //Make sure review belongs to user or user is admin
-   if(review.user.toString() !== req.user.id && req.user.role !== 'admin'){
-    return next(new ErrorResponse(`Not authorised to update review`,401))
-   }
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorised to update review`, 401))
+    }
 
-   review = await Review.findByIdAndUpdate(req.params.id,req.body,{
-    new: true,
-    runValidators: true
-   })
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
 
     res.status(200).json({
         success: true,
         data: review
     })
 })
+
+//@desc Delete review
+//@route DELETE /api/v1/reviews/:id
+//@access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id);
+  
+    if (!review) {
+      return next(
+        new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+      );
+    }
+  
+    // Make sure review belongs to user or user is admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+      return next(new ErrorResponse(`Not authorized to update review`, 401));
+    }
+  
+    await review.deleteOne();
+  
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  });
+  
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const {getReviews,getReview,addReview,updateReview} = require('../controllers/reviews')         
+const {getReviews,getReview,addReview,updateReview,deleteReview} = require('../controllers/reviews')         
 const Review = require('../Models/Review')    
 const router = express.Router({mergeParams: true})    
 
@@ -10,5 +10,5 @@ const {protect,authorize} = require('../middleware/auth')
 router.route('/').get(advancedResults(Review,{path:'bootcamp',select:'name description'})
 ,getReviews).post(protect,authorize('user','admin'),addReview)
 
-router.route('/:id').get(getReview).put(protect,authorize('user','admin'),updateReview)
+router.route('/:id').get(getReview).put(protect,authorize('user','admin'),updateReview).delete(protect, authorize('user', 'admin'), deleteReview)
 module.exports = router

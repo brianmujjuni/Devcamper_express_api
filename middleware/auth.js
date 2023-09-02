a7092b6778dc43d34933b6b67d2858cfce240875
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-02 14:02:05 +0300

    resolve an used imports

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
 const asyncHandler = require('./async')
-const errorResponse = require('../utils/errorResponse')
 const User = require('../Models/User')
 const ErrorResponse = require('../utils/errorResponse')
 
@@ -10,9 +9,9 @@ exports.protect = asyncHandler(async(req,res,next)=>{
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
         token = req.headers.authorization.split(' ')[1]
-     }//else if(req.cookies.token){
-    //     token = req.cookies.token
-    // }
+     }else if(req.cookies.token){
+         token = req.cookies.token
+     }
 
     //Make sure token is sent or exists
     if(!token){

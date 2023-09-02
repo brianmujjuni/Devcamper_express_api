171ed68cbf5579fc4c560dc0e5433fe777b95fe0
Mujjuni Brian <brianmujjuni@engineer.com>
2023-09-02 15:39:32 +0300

    added cors

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ const helmet = require('helmet')
 const xss = require('xss-clean')
 const hpp = require('hpp')
 const ratelimit = require('express-rate-limit')
+const cors = require('cors')
+
 const app = express()
 
 //load env vars
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 //connect to database 
 connectDb()
@@ -42,9 +44,15 @@ app.use(xss())
 //Rate limiting
 const limiter = ratelimit({
     windowMs: 10 * 60 * 1000, //10 mins
-    max: 1
+    max: 100
 })
 app.use(limiter)
+
+//Prevent http param pollution
+app.use(hpp())
+
+//Enable CORS
+app.use(cors())
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
@@ -55,27 +63,27 @@ app.use(express.json())
 app.use(cookieParser())
 
 //Dev logging middleware
-if(process.env.NODE_ENV == 'development'){
+if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'))
 }
 
 //Mount routers
-app.use('/api/v1/bootcamps',bootcamps)
-app.use('/api/v1/courses',courses)
-app.use('/api/v1/users',users)
-app.use('/api/v1/auth',auth)
-app.use('/api/v1/reviews',reviews)
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+app.use('/api/v1/users', users)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/reviews', reviews)
 
 //middle ware
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold ))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 //handle unhandled promise rejection 
-process.on('unhandledRejection',(err,promise)=>{
+process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red)
     //close server and exist process
-    server.close(()=>promise.exit(1))
+    server.close(() => promise.exit(1))
 })
